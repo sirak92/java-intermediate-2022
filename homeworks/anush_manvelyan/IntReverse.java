7daeb939aab6eda3a89f559b6f0f3385d1c7f27b
anushmanvelyan <anush.manvelyan935@gmail.com>
2022-03-27 15:49:59 +0400

    IntReverse

---
+++ homeworks/anush_manvelyan/IntReverse.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class IntReverse{
+	public static void main(String[] args){
+
+		Scanner input = new Scanner(System.in);
+
+		Reversal num;
+
+		System.out.print("\nEnter a number: ");
+		num.number = input.nextInt();
+
+		System.out.println("\n" + Reversal.reverse(num.number));
+		System.out.println("\nBy default function result: ");
+		System.out.println(Integer.reverse(num.number));
+	}
+}
+
+class Reversal{
+	int number;
+
+	static int reverse(int number){
+		int rev = 0;
+
+		while(number > 0)
+		rev *= 10;
+		rev += number % 10;
+		number /= 10;
+
+		return rev;
+	}
+}

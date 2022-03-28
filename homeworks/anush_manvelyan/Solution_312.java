430b32b5e08fd9e36d41fff5912be9d0337b8df3
anushmanvelyan <anush.manvelyan935@gmail.com>
2022-03-28 17:09:54 +0400

    Solution_312

--- homeworks/anush_manvelyan/Solution_312.java
+++ homeworks/anush_manvelyan/Solution_312.java
@@ -22,6 +22,7 @@ public class Solution_312 {
         }
 
         int j = 0;
+        
         for (int i = 0; i < n - 1; ++i){
             if (Math.abs(X[i]) > Math.abs(X[i + 1]))
                 Y[j++] = X[i];

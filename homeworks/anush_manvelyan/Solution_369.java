5d5e7ab43bd52ebc6682d3c5210dfd1687f5e84b
anushmanvelyan <anush.manvelyan935@gmail.com>
2022-03-28 15:03:52 +0400

    Solution_369

--- homeworks/anush_manvelyan/Solution_369.java
+++ homeworks/anush_manvelyan/Solution_369.java
@@ -3,6 +3,7 @@ import java.util.Random;
 public class Solution_369 {
 
     public static void sorting(int arr[], int length) {
+
         for (int i = 0; i < length - 1; ++i)
             for (int j = i; j >= 0; --j)
                 if (arr[j] > arr[j + 1]) {
@@ -42,12 +43,14 @@ public class Solution_369 {
         System.out.println();
 
         boolean t = true;
+
         for (int i = 0; i < 30; ++i) {
             for (int j = 0; j < 30; ++j)
                 if (X[i] == Y[j]) {
                     t = false;
                     break;
                 }
+                
             if (t) {
                 System.out.println(X[i]);
                 break;

5dd58e788824187cc63cdfc7891d9736826d9f43
Edgar Meloyan <edgar7773600@gmail.com>
2022-03-30 11:31:57 +0400

    IntegerDemo changes:

--- homeworks/edgar_meloyan/Homework 3/IntegerDemo.java
+++ homeworks/edgar_meloyan/Homework 3/IntegerDemo.java
@@ -9,53 +9,53 @@ public class IntegerDemo {
         System.out.print("Enter the second number for the first Integer operation: ");
         int num2 = console.nextInt();
         IntegerOperations obj1 = new IntegerOperations(num1, num2);
-        IntegerOperations.integerEquals();
+        obj1.integerEquals();
 
         System.out.print("Enter the first number for the second Integer operation: ");
         int num3 = console.nextInt();
         System.out.print("Enter the second number for the second Integer operation: ");
         int num4 = console.nextInt();
         Max obj2 = new Max(num3, num4);
-        Max.max();
+        obj2.max();
 
         System.out.print("Enter the first number for the third Integer operation: ");
         int num5 = console.nextInt();
         System.out.print("Enter the second number for the third Integer operation: ");
         int num6 = console.nextInt();
         Comparison obj3 = new Comparison(num5, num6);
-        Comparison.compare();
+        obj3.compare();
     }
 }
 
 
 class IntegerOperations {
-    private static int a, b;
+    int a, b;
 
     IntegerOperations(int num1, int num2) {
         a = num1;
         b = num2;
     }
 
-    static void integerEquals() {
-        if (a > b) {
-            System.out.println("The first number is greater then the second");
-        } else if (a < b) {
-            System.out.println("The first number is less then the second");
+    boolean integerEquals() {
+        if (a == b) {
+            System.out.println("True!");
+            return true;
         } else {
-            System.out.println("Both numbers are equal");
+            System.out.println("False!");
+            return false;
         }
     }
 }
 
 class Max {
-    private static int a, b;
+    int a, b;
 
     Max(int a, int b) {
-        Max.a = a;
-        Max.b = b;
+        this.a = a;
+        this.b = b;
     }
 
-    static int max() {
+    int max() {
         int max = a;
         System.out.println(a + " has the maximum value!");
         if (max < b) {
@@ -67,20 +67,22 @@ class Max {
 }
 
 class Comparison {
-    private static int num1, num2;
+    int num1, num2;
 
     Comparison(int a, int b) {
-        Comparison.num1 = a;
-        Comparison.num2 = b;
+        this.num1 = a;
+        this.num2 = b;
     }
 
-    static int compare() {
+    int compare() {
+    	int result;
         if (num1 > num2) {
-            return 1;
+            result = 1;
         } else if (num1 == num2) {
-            return 0;
+            result = 0;
         } else {
-            return -1;
+            result = -1;
         }
+        return result;
     }
-}
+} 

9b93ee3ac27ae48e33af3111acb2e9b567b55d66
Edgar Meloyan <edgar7773600@gmail.com>
2022-04-09 05:17:39 +0400

    Delete file

--- homeworks/edgar_meloyan/Homework 5/Reverse.java
+++
@@ -1,8 +0,0 @@
-public class Reverse {
-	public static void main(String[] args) {
-		StringBuilder sBuild = new StringBuilder("Hello world!");
-		System.out.println(sBuild);
-		sBuild.reverse();
-		System.out.println(sBuild);
-	}
-}

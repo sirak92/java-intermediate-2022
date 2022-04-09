7b7fe066f20238de8310b7d0551fd6d00262022c
Edgar Meloyan <edgar7773600@gmail.com>
2022-04-09 07:10:54 +0400

    Edgar Meloyan: Homework 6

---
+++ homeworks/edgar_meloyan/Homework 6/Computers.java
@@ -0,0 +1,13 @@
+public class Computers {
+    int ram;
+    String cpu;
+    String companyName;
+    String graficCard;
+
+    public Computers(int ram, String cpu, String companyName, String graficCard) {
+        this.ram = ram;
+        this.cpu = cpu;
+        this.companyName = companyName;
+        this.graficCard	= graficCard;
+    }
+}
---
+++ homeworks/edgar_meloyan/Homework 6/Lenovo.java
@@ -0,0 +1,14 @@
+public class Lenovo extends Computers {
+    String model;
+
+    public Lenovo(String companyName, String model, int ram, String cpu, String graficCard) {
+        super(ram, cpu, companyName, graficCard);
+        this.model = model;
+    }
+
+
+    void showTheComputer() {
+        System.out.printf("%-15s%-25s%-10d%-25s%-18s%n", companyName, model, ram, cpu, graficCard);
+    }
+}
+

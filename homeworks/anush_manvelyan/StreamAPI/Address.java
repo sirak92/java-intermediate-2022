7a72514fffce7bd0b79851a0a1a2220ddedc4db2
anushmanvelyan <anush.manvelyan935@gmail.com>
2022-05-27 22:12:57 +0400

    Homework about StreamAPI

---
+++ homeworks/anush_manvelyan/StreamAPI/Address.java
@@ -0,0 +1,17 @@
+package tech42;
+
+class Address {
+    private String zipcode;
+
+    public Address(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+}
